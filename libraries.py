4b92dccebc9bb3c5752efdda66e1ce1efaf384d3
OMID MAHMOODI <omid.mahmoudi65@gmail.com>
2020-02-18 11:41:36 -0600

    Add files via upload

---
+++ libraries.py
@@ -0,0 +1,15 @@
+!pip install geosoft==9.2
+!pip install geosoft --upgrade
+import geosoft
+import geosoft.gxpy.gx as gx
+gxp = gx.GXpy()
+print(gxp.gid)
+exit()
+
+ MyGX.GX
+gxp = gx()
+gxx=gx.GXpy()
+A= gxx.entitlements()
+print(gxx.entitlements)
+
+help(gx)
