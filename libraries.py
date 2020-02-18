7b59e2266b2184a909f1af74b1fbea1ffae75cb2
OMID MAHMOODI <omid.mahmoudi65@gmail.com>
2020-02-18 11:43:17 -0600

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -4,12 +4,4 @@ import geosoft
 import geosoft.gxpy.gx as gx
 gxp = gx.GXpy()
 print(gxp.gid)
-exit()
-
- MyGX.GX
-gxp = gx()
-gxx=gx.GXpy()
-A= gxx.entitlements()
-print(gxx.entitlements)
-
-help(gx)
+print(gxp.entitlements)
